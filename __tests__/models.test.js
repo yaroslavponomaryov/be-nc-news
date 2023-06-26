9025fe567152eb0b17a55e285720eb80a74cd597
Yaroslav Ponomaryov <yaroslavponomaryov@gmail.com>
2023-06-26 16:55:23 +0100

    added GET/api/articles/:article_id happy path test

--- __tests__/models.test.js
+++ __tests__/models.test.js
@@ -43,7 +43,7 @@ describe('GET/api/', () => {
   });
 });
 
-describe('GET//api/articles/:article_id', () => {
+describe('GET/api/articles/:article_id', () => {
   test('200: responds with particular article by ID provided id', () => {
     return request(app)
       .get('/api/articles/1')
@@ -51,7 +51,13 @@ describe('GET//api/articles/:article_id', () => {
       .then(({ body }) => {
         const { article } = body;
         expect(article).toHaveProperty('article_id', expect.any(Number));
-
+        expect(article).toHaveProperty('title', expect.any(String));
+        expect(article).toHaveProperty('topic', expect.any(String));
+        expect(article).toHaveProperty('author', expect.any(String));
+        expect(article).toHaveProperty('body', expect.any(String));
+        expect(article).toHaveProperty('created_at', expect.any(String));
+        expect(article).toHaveProperty('votes', expect.any(Number));
+        expect(article).toHaveProperty('article_img_url', expect.any(String));
       });
 
   });
